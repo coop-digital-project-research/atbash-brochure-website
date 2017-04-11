91d8784cf07da96e6b521ebb0576e3dce156ccb0
Ian Drysdale <idrysdale@gmail.com>
2017-04-11 15:25:44 +0100

    Add a prompt for the chrome button

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,34 +1,19 @@
-function createCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name,"",-1);
-}
-
 document.addEventListener("DOMContentLoaded", function(event) {
-  var accessCount = readCookie('accessCount')
-  if (accessCount) {
-    console.log("Been accessed before")
-    createCookie('accessCount',1,7);
-  } else {
-    console.log("First access")
+  var addToChromePrompt = document.getElementById('add-to-chrome-prompt');
+
+  if (addToChromePrompt) {
+
+    dismissButton = document.getElementById('button-bar')
+    dismissButton.addEventListener('click', function() {
+        addToChromePrompt.style.display = 'none';
+    })
+
+    addToChromePrompt.style.display = 'none';
+
+    button = document.getElementById('add-to-chrome-button');
+
+    button.addEventListener('click', function() {
+        addToChromePrompt.style.display = 'block';
+    });
   }
 });
